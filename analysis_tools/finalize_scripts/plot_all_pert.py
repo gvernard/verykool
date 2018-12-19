Fixed calculating the convergence (the denominator of the second numerical derivative was missing), and 'source_irregular.dat' is computed within the mask (for the source).

analysis_tools/finalize_scripts/plot_all_pert.py
@@ -331,6 +331,7 @@ plt.colorbar(col,cax=cax,format="%6.4f")
 if os.path.isfile(data_path+'perturbations.fits'):
     im_pert_true = fits.getdata(data_path+'perturbations.fits',ext=0)
     im_pert_true = im_pert_true[::-1,:]
+#    im_pert_true /= np.power(6.05/121,2)
     max_pert_true = np.amax([abs(np.amax(im_pert_true)),abs(np.amin(im_pert_true))])
 else:
     max_pert_true = 0
@@ -343,11 +344,10 @@ else:
     im_pert = fits.getdata(out_path+'pert_dpsi.fits',ext=0)
     pert_title = "RECONSTRUCTION"
 im_pert = im_pert[::-1,:]
+#im_pert /= np.power(6.05/40,2)
 
 # Set color scale
 limit = np.amax([max_pert_true,abs(np.amax(im_pert)),abs(np.amin(im_pert))])
-
-
 
 # Plot original perturbations if they exist
 if os.path.isfile(data_path+'perturbations.fits'):

analysis_tools/finalize_scripts/plot_source_pow_spec.py
@@ -6,6 +6,14 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+def filterZero(x,y,e):
+    ind = np.nonzero(y)
+    bins = x[ind]
+    vals = y[ind]
+    errs = e[ind]
+    return bins,vals,errs
+
+
 
 path = sys.argv[1]
 run  = sys.argv[2]
@@ -31,12 +39,14 @@ plt.yscale('log')
 os.system("./create_gridded_source "+path+" "+run+" 200 "+lmodel+" "+step)
 os.system("python plot_pow_spec.py source_model.fits")
 x,y,e = np.loadtxt("ps.dat",unpack=True)
+x,y,e = filterZero(x,y,e)
 plt.errorbar(x,y,yerr=e,label='reconstruction',color='blue')
 
 # true source
 if os.path.isfile(path+"data/source.fits"):
     os.system("python plot_pow_spec.py "+path+"data/source.fits")
     x,y,e = np.loadtxt("ps.dat",unpack=True)
+    x,y,e = filterZero(x,y,e)
     plt.errorbar(x,y,yerr=e,label='truth',color='black')
 
 plt.legend()
@@ -49,4 +59,4 @@ plt.savefig('source_ps.pdf',bbox_inches='tight')
 
 os.remove("ps.dat")
 os.remove("ps.pdf")
-os.remove("source_model.fits")
+#os.remove("source_model.fits")